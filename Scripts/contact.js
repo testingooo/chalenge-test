a4e3edca4ddba71f7b9b0008d11616b138a7441a
testingooo <warisganiu201@gmail.com>
2024-07-15 22:58:25 +0100

    Add files via upload

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -1,3 +1,22 @@
 import { header } from "./Utillis/header.js";
 
-header();
+header;
+
+document.querySelector("form").addEventListener("click", (e) => {
+  const name = document.querySelector(".name");
+  const email = document.querySelector(".email");
+  const phone = document.querySelector(".phone");
+  const message = document.querySelector(".message");
+
+  const nameError = document.querySelector(".name-error");
+  const emailError = document.querySelector(".email-error");
+  const phoneError = document.querySelector(".phone-error");
+  const messageError = document.querySelector(".message-error");
+
+  e.preventDefault();
+  if ("") {
+    console.log("error");
+  } else {
+    console.log("ok");
+  }
+});
